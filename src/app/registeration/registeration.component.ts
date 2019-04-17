73af8797c0dddb11ca8f3dc415aa245cb9e501ba
Mohamed Maadnous <moh_759@hotmail.com>
2019-04-17 15:47:25 +0200

    /

--- src/app/registeration/registeration.component.ts
+++ src/app/registeration/registeration.component.ts
@@ -1,25 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthenticationService } from '@app/_services';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { AuthenticationService } from '@app/_services'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-registeration',
   templateUrl: './registeration.component.html',
-  styleUrls: ['./registeration.component.css']
+  styleUrls: ['./registeration.component.css'],
 })
 export class RegisterationComponent implements OnInit {
   registerForm: FormGroup
   loading = false
   submitted = false
   response
-  mustmatch: boolean = false;
+  notSame = true
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
-    private authenticationService: AuthenticationService,
+    private authenticationService: AuthenticationService
   ) {}
+
+  checkPasswords(f: FormGroup) {
+    // here we have the 'passwords' group
+    let pass = f.controls.password
+    let confirmPass = f.controls.confirmPassword
+
+    return pass === confirmPass ? null : { notSame: true }
+  }
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group(
@@ -33,6 +41,7 @@ export class RegisterationComponent implements OnInit {
         /** confirm Password here */
         confirmPassword: ['', Validators.required],
       },
+      { validator: this.checkPasswords }
     )
   }
 
@@ -41,27 +50,37 @@ export class RegisterationComponent implements OnInit {
     return this.registerForm.controls
   }
 
+  get c() {
+    return this.registerForm
+  }
+
   onSubmit() {
     this.submitted = true
-
+    this.notSame = this.registerForm.errors.notSame
     // stop here if form is invalid
+    // if (this.registerForm.invalid) {
+    //   if (this.f.password.value !== this.f.confirmPassword.value) {
+    //     this.mustmatch = true
+    //   }
+    //   return
+    // }
+    // console.log('after')
+    // function qui va communiquer avec le service
+
     if (this.registerForm.invalid) {
-      if(this.f.password.value !== this.f.confirmPassword.value) {
-        this.mustmatch = true
-      }
+      console.log(this.registerForm.errors)
+      console.log(this.registerForm.errors.notSame)
       return
     }
-    console.log("after")
-    // function qui va communiquer avec le service
-        this.authRequest(this.registerForm.value)
+    this.authRequest(this.registerForm.value)
   }
 
   authRequest(data) {
     // fournir les donnes au service pour enregistrer l'user
     this.authenticationService.register(data).subscribe(res => {
       console.log(res)
-      this.response = res as string []
-      if(this.response.status == "saved") {
+      this.response = res as string[]
+      if (this.response.status == 'saved') {
         localStorage.setItem('user', this.response.data)
         this.router.navigate(['home'])
       }
